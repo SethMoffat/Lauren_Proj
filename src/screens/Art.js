929ca55dd8238c6ad50432332b34d0e3db951a9b
Seth <sethmoffat1@gmail.com>
2022-11-16 02:40:19 -0700

    spaced links

--- src/screens/Art.js
+++ src/screens/Art.js
@@ -8,7 +8,9 @@ function Art() {
     <h4>Pick a career that you find to be intersting to you!</h4>
 
     <Link to="/artist">Artist</Link>
+    <p> </p>
     <Link to="/musician">Musician</Link>
+    <p> </p>
     <Link to="/photographer">Photographer</Link>
     
 </body>
